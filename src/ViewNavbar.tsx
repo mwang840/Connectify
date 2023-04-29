624ced1f68a69b97f743e1a264d58f5a4dadcb19
mwang840 <maxwang@udel.edu>
2023-04-29 16:19:18 -0400

    added navbar to app no links turning purple

--- src/ViewNavbar.tsx
+++ src/ViewNavbar.tsx
@@ -5,7 +5,7 @@ import {Nav, Navbar} from "react-bootstrap";
 export function ViewNavbar(): JSX.Element {
     return (
         
-          <Navbar expand="lg" style={{backgroundColor: "#ADD8E6", paddingLeft:"15px", color: "white", paddingRight: "15px"}} className="justify-content-center">
+          <Navbar expand="lg" style={{backgroundColor: "#ADD8E6", paddingLeft:"15px", color: "white", paddingRight: "15px", fontSize: "40px"}} className="justify-content-center">
           
             <Navbar.Brand style={{position: "relative", left: "0px", color: "black", textAlign: "center"}}>
             Connectify
@@ -13,13 +13,13 @@ export function ViewNavbar(): JSX.Element {
             <Navbar.Collapse id="basic-navbar-nav" className="d-flex align-items-center">
             <Nav className="flex-grow-1 justify-content-evenly">
               <Nav.Item style={{textAlign: "center", margin: "20px", fontFamily:"quicksand"}}>
-              <Nav.Link style={{ margin: "20px"}} href="/home">
+              <Nav.Link style={{ margin: "20px", textDecoration: "none", color: "black"}} href="/home" target="_blank">
                 Home  
               </Nav.Link>
-              <Nav.Link style={{ margin: "20px", fontFamily: "quicksand"}} href="/Profile">
+              <Nav.Link style={{ margin: "20px", fontFamily: "quicksand", textDecoration: "none", color: "black"}} href="/Profile" target="_blank">
                 Profile
               </Nav.Link>
-              <Nav.Link style={ {margin: "20px", fontFamily: "quicksand"}} href="/Explore">
+              <Nav.Link style={ {margin: "20px", fontFamily: "quicksand", textDecoration: "none", fontSize: "40px", color: "black"}} href="/Explore" target="_blank">
                 Explore
               </Nav.Link>
               </Nav.Item>
