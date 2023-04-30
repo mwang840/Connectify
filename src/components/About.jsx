10d101a024033147adfc66ad536e4dcd0f034758
CarlsonCarlson <carlson.cheng888@gmail.com>
2023-04-30 11:40:20 -0400

    final commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,9 +30,29 @@ function About(props: { user_id: string; }) {
         {hobbies[i].emoji} {hobbies[i].name} 
       </p>)
     }
+    let hobbyPosts = [];
+    if (props.user_id == 0) {
+      hobbyPosts.push(
+        <div>
+        <h1 className="title_post"> Photography </h1>
+        <p> I love taking pictures of nature and people. </p>
+        <p> I went to peru over winter 2023! </p>
+        <br></br>
+        <h1 className="title_post"> Cooking </h1>
+        <p> I love cooking! </p>
+        <p> Baking scones brings meaning to my life! </p>
+        <br></br>
+        <h1 className="title_post"> Hiking </h1>
+        <p> Quebec Trail Running Race 4/23/2023 </p>
+        <p> Got 6th place out of 25 runners </p>
+        </div>
+      )
+    }
     return (
       <div className="about">
         {hobbyElements}
+        <br></br>
+        {hobbyPosts}
       </div>
     );
   }
--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,23 +1,75 @@
 import React, { Component } from 'react'
 import { useResolvedPath } from 'react-router-dom';
-import pic from './profile-pictures/3f.jpg';
+import pic0 from './profile-pictures/0f.jpg';
+import pic1 from './profile-pictures/0m.jpg';
+import pic2 from './profile-pictures/1f.jpg';
+import pic3 from './profile-pictures/1m.jpg';
+import pic4 from './profile-pictures/2f.jpg';
+import pic5 from './profile-pictures/2m.jpg';
+import pic6 from './profile-pictures/3f.jpg';
+import pic7 from './profile-pictures/3m.jpg';
+import pic8 from './profile-pictures/4f.jpg';
+import pic9 from './profile-pictures/4m.jpg';
 import { serialized } from './About';
 
 //import pfp from data.users[0].profile_picture;
 
 function Name(props: { user_id: string; serialized: [];}) {
     let name = serialized[props.user_id].name;
+    let pfp = [];
+    if (props.user_id == 0) {
+        pfp.push(
+            <img className="profilepic" src={pic0} alt="0"></img>
+        )
+    }
+    if (props.user_id == 1) {
+        pfp.push(
+            <img className="profilepic" src={pic1} alt="1"></img>
+        )
+    }
+    if (props.user_id == 2) {
+        pfp.push(
+            <img className="profilepic" src={pic2} alt="2"></img>
+        )
+    }
+    if (props.user_id == 3) {
+        pfp.push(
+            <img className="profilepic" src={pic3} alt="3"></img>
+        )
+    }
+    if (props.user_id == 4) {
+        pfp.push(
+            <img className="profilepic" src={pic4} alt="4"></img>
+        )
+    }
+    if (props.user_id == 5) {
+        pfp.push(
+            <img className="profilepic" src={pic5} alt="5"></img>
+        )
+      }
+    if (props.user_id == 6) {
+        pfp.push(
+            <img className="profilepic" src={pic6} alt="6"></img>
+        )
+    }
+    if (props.user_id == 7) {
+        pfp.push(
+            <img className="profilepic" src={pic7} alt="7"></img>
+        )
+    }
+    if (props.user_id == 8) {
+        pfp.push(
+            <img className="profilepic" src={pic8} alt="8"></img>
+        )
+        }
+    if (props.user_id == 9) {
+        pfp.push(
+            <img className="profilepic" src={pic9} alt="9"></img>
+        )
+    }
         return (
             <section id="container-about" className="container-about">
-                    <img className="profilepic" src={pic}
-                        style={{
-                            margin: 'auto',
-                            borderColor: 'white',
-                            // borderwidth: 5,
-                            // width: 200,
-                            // height: 200,
-                        }}
-                        alt="profilepic" />
+                    {pfp}
                     <h1> {name} </h1>
                 </section>
         )
