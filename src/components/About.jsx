245219ac28e53fa28754de8fb265aff76b8acfe1
Isaac Lewis <isaaclewis@wifi-roaming-128-4-105-199.wifi.udel.edu>
2023-04-29 21:23:56 -0400

    added profile functionality

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import data from './users.json';
+
+/*
+interface Hobbies {
+    name: string
+    emoji: string
+}
+*/
+
+let hobbies = "";
+for(let i=0; i<data.users[0].hobbies.length; i++){
+    hobbies = hobbies.concat("\n", data.users[0].hobbies[i].name + data.users[0].hobbies[i].emoji);
+}
+//let hobbies = data.users[0].hobbies[0].name;
 
 class About extends Component {
     render() {
         return (
             <section id="container-about" className="container-about">
-            <h1> Hobbies </h1>
-            <p> hobbies </p>
+            <h3> Hobbies </h3>
+            <p> {hobbies} </p>
             </section>
         )
     }
--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,36 +1,27 @@
 import React, { Component } from 'react'
-import {StyleSheet} from 'react-native';
-import logo from './logo.svg';
+import { useResolvedPath } from 'react-router-dom';
+import data from './users.json';
+import pic from './profile-pictures/3f.jpg';
+import './name.css';
 
-class Name extends Component {
-    render() {
+let name = data.users[0].name;
+//import pfp from data.users[0].profile_picture;
+
+function Name() {
         return (
             <section id="container-about" className="container-about">
-            <view style={styles.container}>
-            <img id="profilepic" src={logo}
-            style={{
-             borderColor: 'white',
-             borderwidth: 5,
-             width: 240,
-              height: 240,
-            }}
-             alt="profilepic"/>
-             </view>
-            <h1> Name </h1>
-            </section>
+                    <img id="profilepic" src={pic}
+                        style={{
+                            borderColor: 'white',
+                            borderwidth: 5,
+                            width: 200,
+                            height: 200,
+                            borderRadius: 200 / 2
+                        }}
+                        alt="profilepic" />
+                    <h1> {name} </h1>
+                </section>
         )
     }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100%',
-        textAlign: 'center',
-    }
-})
 
 export default Name
