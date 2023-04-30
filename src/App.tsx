e44ee75095932fe42ec499b81f2da9b9573ccbb0
CarlsonCarlson <carlson.cheng888@gmail.com>
2023-04-29 20:48:53 -0400

    home page and tailwindcss integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/hello_world'
+// import Welcome from './components/value-prop';
+import ValueProp from './components/value-prop';
+import { ReactComponent as Users_svg } from './assets/Users.svg';
+import { ReactComponent as UserSwitch_svg } from './assets/UserSwitch.svg';
+import { ReactComponent as HandWaving_svg } from './assets/HandWaving.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Value-propositions">
+      {/* <HelloWorld /> */}
+      <ul>
+        <HandWaving_svg />
+        <ValueProp title="Your place to share your passions." 
+          text="One profile. Your passions." />
+        <Users_svg />
+        <ValueProp title="Better connect with acquaintances, classmates & co-workers. " 
+          text="Learn about their interests. Find something to talk about." />
+        <UserSwitch_svg />
+        <ValueProp title="Just met? Exchange profiles. Learn each other's interests." 
+          text="Meet new people with shared interests." />
+      </ul>
     </div>
+    // <div>
+    // </div>
   );
 }
 
